d92ef39178c9beac70926002e349b055ec2e390a
Marvki <marvkipi@gmail.com>
2016-06-15 20:35:00 -0700

    add new note

--- js/Note.js
+++ js/Note.js
@@ -57,12 +57,14 @@ var Board = React.createClass({
   getInitialState: function() {
     return {
       notes: [
-        "Call Bill",
-        "Email Lisa",
-        "Make dentist appt",
-        "Send Proposal"
+
       ]
     };
+  },
+  add: function(text) {
+    var arr = this.state.notes;
+    arr.push(text);
+    this.setState({notes: arr})
   },
   update: function(newText, i) {
     var arr = this.state.notes;
@@ -86,6 +88,8 @@ var Board = React.createClass({
   render: function() {
     return (<div className="board">
           {this.state.notes.map(this.eachNote)}
+          <button className="btn btn-sm btn-success glyphicon glyphicon-plus"
+              onClick={this.add.bind(null, "enter text")}/>
         </div>
     );
   }
